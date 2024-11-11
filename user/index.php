30a7c37ff4a0cfe0642f32a0b00b577cc85ccf78
doanlc <doanlcph48630@gmail.com>
2024-11-11 12:50:16 +0700

    update code

--- user/index.php
+++ user/index.php
@@ -1,26 +1,15 @@
-<!-- TRANG INDEX DÙNG ĐỂ THAO TÁC  -->
+<!-- TRANG INDEX DÙNG ĐỂ THAO TÁC -->
 <?php
 session_start();
 include "./controller/ProductController.php";
-$action = isset($_GET['action']) ? $_GET['action'] : 'home';
+$action = $_GET['action'] ?? 'home';
 $productcontroller = new ProductController;
-switch ($action) {
-  case "home":
-    $productcontroller->showAll();
-    break;
 
-  case "product-detail":
-    $id = $_GET['id'];
-    $category_id = $_GET['category_id'];
-    $productcontroller->productDetail($id, $category_id);
-    break;
-  case "product-category":
-    $id = $_GET['id'];
-    $productcontroller->productCategory($id);
-    break;
-  case "product":
-    $productcontroller->product();
-    break;
-
-}    
+match ($action) {
+  'home' => $productcontroller->showAll(),
+  'product-detail' => $productcontroller->productDetail($_GET['id'], $_GET['category_id']),
+  'product-category' => $productcontroller->productCategory($_GET['id']),
+  'product' => $productcontroller->product(),
+  default => throw new Exception("Action not recognized")
+};
 ?>
